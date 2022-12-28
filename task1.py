1e12b820b15687c39ca81f868a47fb44775c9cf0
Шекунов Андрей <ashekunov@gmail.com>
2022-12-29 01:51:14 +0500

    Задача 1 - Вычисления арифметического выражения заданного строкой

--- task1.py
+++ task1.py
@@ -1,33 +1,60 @@
 # Задача 1. Напишите программу вычисления арифметического выражения заданного строкой.
 # Используйте операции +,-,/,*. приоритет операций стандартный.
 # Например:
-# 2+2 => 4;
-# 1+2*3 => 7;
-# 1-2*3 => -5
+# 2 + 2 => 4;
+# 1 + 2 * 3 => 7;
+# 1 - 2 * 3 => -5
 
-num = '1 * 2 * 3 / 5'
-print(f'{num} =', eval(num))
-
+num = '2 + 2'
 num = num.split()
+
 print(num)
+
 while len(num) > 1:
-    while '*' in num or '/' in num :
+    while '*' in num or '/' in num:
         if num.count('*') > 0 and num.count('/') > 0:
             if num.index('/') > num.index('*'):
-                num[num.index('*') - 1] = int(num[num.index('*') - 1]) * int(num[num.index('*') + 1])
+                num[num.index('*') - 1] = int(num[num.index('*') - 1]
+                                              ) * int(num[num.index('*') + 1])
                 num.pop(num.index('*') + 1)
                 num.pop(num.index('*'))
             else:
-                num[num.index('/') - 1] = int(num[num.index('/') - 1]) / int(num[num.index('/') + 1])
+                num[num.index('/') - 1] = int(num[num.index('/') - 1]
+                                              ) / int(num[num.index('/') + 1])
                 num.pop(num.index('/') + 1)
                 num.pop(num.index('/'))
         else:
             if '*' in num:
-                num[num.index('*') - 1] = int(num[num.index('*') - 1]) * int(num[num.index('*') + 1])
+                num[num.index('*') - 1] = int(num[num.index('*') - 1]
+                                              ) * int(num[num.index('*') + 1])
                 num.pop(num.index('*') + 1)
                 num.pop(num.index('*'))
             else:
-                num[num.index('/') - 1] = int(num[num.index('/') - 1]) / int(num[num.index('/') + 1])
+                num[num.index('/') - 1] = int(num[num.index('/') - 1]
+                                              ) / int(num[num.index('/') + 1])
                 num.pop(num.index('/') + 1)
                 num.pop(num.index('/'))
+    while '+' in num or '-' in num:
+        if num.count('+') > 0 and num.count('-') > 0:
+            if num.index('-') > num.index('+'):
+                num[num.index('+') - 1] = int(num[num.index('+') - 1]
+                                              ) + int(num[num.index('+') + 1])
+                num.pop(num.index('+') + 1)
+                num.pop(num.index('+'))
+            else:
+                num[num.index('-') - 1] = int(num[num.index('/') - 1]
+                                              ) - int(num[num.index('-') + 1])
+                num.pop(num.index('-') + 1)
+                num.pop(num.index('-'))
+        else:
+            if '+' in num:
+                num[num.index('+') - 1] = int(num[num.index('+') - 1]
+                                              ) + int(num[num.index('+') + 1])
+                num.pop(num.index('+') + 1)
+                num.pop(num.index('+'))
+            else:
+                num[num.index('-') - 1] = int(num[num.index('-') - 1]
+                                              ) - int(num[num.index('-') + 1])
+                num.pop(num.index('-') + 1)
+                num.pop(num.index('-'))
 print(num)
